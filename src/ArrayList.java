5a23cdf7fce26c803f5a7d697f19218235c9a854
James <jamesghill@yahoo.co.uk>
2015-01-10 22:55:12 +0000

    Coded add with single item
    Created first draft of this add method

---
+++ src/ArrayList.java
@@ -0,0 +1,44 @@
+public class ArrayList implements List {
+    
+    private Object[] newList;
+    private int length;
+    private int lastUsed;
+    
+    public void ArrayList(int size){
+        newList = new Object[size];
+        length = size;
+        lastUsed = 0;
+    }
+    
+    public boolean isEmpty(){
+        
+    }
+    
+    public int size(){
+        
+    }
+    
+    public ReturnObject get(int index){
+        
+    }
+    
+    public ReturnObject remove(int index){
+        
+    }
+    
+    public ReturnObject add(int index, Object item){
+        
+    }
+    
+    public ReturnObject add(Object item){
+        if(item == null){
+            ReturnObjectImpl error = new ReturnObjectImpl(ErrorMessage.INVALID_ARGUMENT);
+            return error;
+        } else {
+            newList[lastUsed] = item;
+            lastUsed =+ 1;
+            ReturnObjectImpl empty = new ReturnObjectImpl();
+            return empty;
+        }
+    }
+}

c7bbf2f0367e054e08a22ee4816ff068e7c8869d
James <jamesghill@yahoo.co.uk>
2015-01-12 17:33:12 +0000

    Added tests
    Increased the number of tests to ensure that ArrayList would work correctly

--- src/Test.java
+++ src/Test.java
@@ -26,7 +26,7 @@ public class Test {
         }
         
         // Test get method
-        if(myArray.get(10).equals(11)){
+        if(myArray.get(10).getReturnValue().equals(10)){
             System.out.println("Test 3 : Working.");
         } else {
             System.out.println("Test 3 : Failed.");
@@ -34,10 +34,44 @@ public class Test {
         
         // Test remove method
         myArray.remove(10);
-        if(!myArray.get(10).equals(11) && myArray.size() == 19){
+        if(myArray.get(10).getReturnValue().equals(11) && myArray.size() == 19){
             System.out.println("Test 4 : Working.");
         } else {
             System.out.println("Test 4 : Failed.");
+        }
+        
+        // Test remove until all deleted
+        // Add objects
+        for(int i = myArray.size()-1; i >= 0; i--){
+            Integer number = i;
+            myArray.remove(number);
+        }
+        if(myArray.size() == 0){
+            System.out.println("Test 5 : Working.");
+        } else {
+            System.out.println("Test 5 : Failed.");
+        }
+        
+        // Test copy to new array
+        for(int i = 0; i < 200; i++){
+            Integer number = i;
+            myArray.add(number);
+        }
+        if(myArray.size() == 200){
+            System.out.println("Test 6 : Working.");
+        } else {
+            System.out.println("Test 6 : Failed.");
+        }
+        
+        // Test adding with index
+        for(int i = 0; i < 10; i++){
+            Integer number = i;
+            myArray.add(number+100, number);
+        }
+        if(myArray.size() == 210 && myArray.get(105).getReturnValue().equals(5)){
+            System.out.println("Test 7 : Working.");
+        } else {
+            System.out.println("Test 7 : Failed.");
         }
         
         

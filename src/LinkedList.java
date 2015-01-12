46b23bba5d250b11234acfa6e7399adb79b4a0eb
James <jamesghill@yahoo.co.uk>
2015-01-12 17:33:58 +0000

    Added basic variables & methods
    Introduced a constructor

---
+++ src/LinkedList.java
@@ -0,0 +1,30 @@
+public class LinkedList implements List {
+    
+    private Object data;
+    private Object next = null;
+    
+    public void LinkedList(Object item){data = item;}
+    
+    public boolean isEmpty(){if (data == null){return true;} else {return false;}}
+    
+    public int size(){
+        
+    }
+    
+    public ReturnObject get(int index){
+        
+    }
+    
+    public ReturnObject remove(int index){
+        
+    }
+    
+    public ReturnObject add(int index, Object item){
+        
+    }
+    
+    public ReturnObject add(Object item){
+        
+    }
+    
+}

d7b4679eed620e293ba3532748f8c629d86cf0a6
James <jamesghill@yahoo.co.uk>
2015-01-18 22:30:43 +0000

    Copied all tests from StackImplTest

--- test/ImprovedStackImplTest.java
+++ test/ImprovedStackImplTest.java
@@ -32,4 +32,40 @@ public class ImprovedStackImplTest {
         assertEquals("Stack is wrong size after popping.", 100, size);
     }
     
+    @Test
+    public void testPush(){
+        FunctionalLinkedList newList = new FunctionalLinkedList();
+        for(int i = 0; i < 100; i++){
+            newList.add(i * 10);
+        }
+        stack = new ImprovedStackImpl(newList);
+        for(int i = 100; i < 200; i++){
+            stack.push(i * 10);
+        }
+        int size = stack.size();
+        assertEquals("New Stack is wrong size.", 200, size);
+    }
+    
+    @Test
+    public void testTop(){
+        FunctionalLinkedList newList = new FunctionalLinkedList();
+        for(int i = 1; i < 101; i++){
+            newList.add(i * 15);
+        }
+        stack = new ImprovedStackImpl(newList);
+        int top = (Integer) stack.top().getReturnValue();
+        assertEquals("Top result is incorrect.", 15, top);
+    }
+    
+    @Test
+    public void testPop(){
+        FunctionalLinkedList newList = new FunctionalLinkedList();
+        for(int i = 1; i < 101; i++){
+            newList.add(i * 15);
+        }
+        stack = new ImprovedStackImpl(newList);
+        stack.pop();
+        int pop = (Integer) stack.pop().getReturnValue();
+        assertEquals("Initial pop did not work correctly.", 30, pop);
+    }
 }

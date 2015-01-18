d95ec91e390165e77d74ad687435ce0c63ee5378
James <jamesghill@yahoo.co.uk>
2015-01-18 22:53:14 +0000

    Created reverse & remove methods

--- src/ImprovedStackImpl.java
+++ src/ImprovedStackImpl.java
@@ -8,12 +8,22 @@ public class ImprovedStackImpl implements ImprovedStack {
     
     @Override
     public ImprovedStack reverse() {
-        
+        List newList = new ArrayList();
+        for(int i = baseList.size()-1; i > -1 ; i--){
+            newList.add(baseList.get(i));
+        }
+        ImprovedStack newStack = new ImprovedStackImpl(newList);
+        return newStack;
     }
 
     @Override
     public void remove(Object object) {
-        
+        for(int i = 0; i < baseList.size(); i++){
+            if(baseList.equals(object)){
+                baseList.remove(i);
+                i = 0;
+            }
+        }
     }
 
     @Override
@@ -23,22 +33,22 @@ public class ImprovedStackImpl implements ImprovedStack {
 
     @Override
     public int size() {
-        
+        return baseList.size();
     }
 
     @Override
     public void push(Object item) {
-        
+        baseList.add(item);
     }
 
     @Override
     public ReturnObject top() {
-        
+        return baseList.get(0);
     }
 
     @Override
     public ReturnObject pop() {
-        
+        return baseList.remove(0);
     }
     
 }

a6e65c8fcbd5a5f4ffc81c789ed068c561a96f86
James <jamesghill@yahoo.co.uk>
2015-01-18 17:44:18 +0000

    Modified original tests & added 2 tests for other List types
    The original tests weren't working so I modified the formulas.  Also added FunctionalArrayList & FunctionalLinkedList tests.

--- test/SampleableListTest.java
+++ test/SampleableListTest.java
@@ -8,12 +8,12 @@ public class SampleableListTest {
     public void SampleArrayListTest(){
         ArrayList myList = new ArrayList();
         for(int i = 1; i < 201; i++){
-            myList.add(i);
+            myList.add(i * 10);
         }
         SampleableListImpl newList = new SampleableListImpl(myList);
         for(int i = 0; i < myList.size(); i++){
             int j = (Integer) newList.get(i).getReturnValue();
-            assertEquals("Sampled numbers are incorrect.", (i+1)*2, j);
+            assertEquals("Sampled numbers are incorrect.", (i+1) * 10, j);
         }
     }
     
@@ -21,12 +21,38 @@ public class SampleableListTest {
     public void SampleLinkedListTest(){
         LinkedList myList = new LinkedList();
         for(int i = 1; i < 201; i++){
-            myList.add(i);
+            myList.add(i * 10);
         }
         SampleableListImpl newList = new SampleableListImpl(myList);
         for(int i = 0; i < myList.size(); i++){
             int j = (Integer) newList.get(i).getReturnValue();
-            assertEquals("Sampled numbers are incorrect.", (i+1)*2, j);
+            assertEquals("Sampled numbers are incorrect.", (i+1) * 10, j);
+        }
+    }
+    
+    @Test
+    public void SampleFunctionalArrayListTest(){
+        FunctionalArrayList myList = new FunctionalArrayList();
+        for(int i = 1; i < 201; i++){
+            myList.add(i * 10);
+        }
+        SampleableListImpl newList = new SampleableListImpl(myList);
+        for(int i = 0; i < myList.size(); i++){
+            int j = (Integer) newList.get(i).getReturnValue();
+            assertEquals("Sampled numbers are incorrect.", (i+1) * 10, j);
+        }
+    }
+    
+    @Test
+    public void SampleFunctionalLinkedListTest(){
+        FunctionalLinkedList myList = new FunctionalLinkedList();
+        for(int i = 1; i < 201; i++){
+            myList.add(i * 10);
+        }
+        SampleableListImpl newList = new SampleableListImpl(myList);
+        for(int i = 0; i < myList.size(); i++){
+            int j = (Integer) newList.get(i).getReturnValue();
+            assertEquals("Sampled numbers are incorrect.", (i+1) * 10, j);
         }
     }
     

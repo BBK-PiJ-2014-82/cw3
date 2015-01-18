756f85c6c80a2b484b7f72912abf8355ed3ccc7e
James <jamesghill@yahoo.co.uk>
2015-01-18 22:04:48 +0000

    Created ImprovedStackImpl class
    Setup all methods to be implemented

---
+++ src/ImprovedStackImpl.java
@@ -0,0 +1,38 @@
+public class ImprovedStackImpl implements ImprovedStack {
+
+    @Override
+    public ImprovedStack reverse() {
+        
+    }
+
+    @Override
+    public void remove(Object object) {
+        
+    }
+
+    @Override
+    public boolean isEmpty() {
+        
+    }
+
+    @Override
+    public int size() {
+        
+    }
+
+    @Override
+    public void push(Object item) {
+        
+    }
+
+    @Override
+    public ReturnObject top() {
+        
+    }
+
+    @Override
+    public ReturnObject pop() {
+        
+    }
+    
+}

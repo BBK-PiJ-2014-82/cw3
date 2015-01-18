e954a0b325ba549595d15425e5eb40378fd60ca4
James <jamesghill@yahoo.co.uk>
2015-01-18 01:44:11 +0000

    Added 'rest' method & changes to 'head'
    'head' was changed to return an error if it is empty.

--- src/FunctionalArrayList.java
+++ src/FunctionalArrayList.java
@@ -1,14 +1,29 @@
-public class FunctionalArrayList extends ArrayList {
+public class FunctionalArrayList extends ArrayList implements FunctionalList {
     
     public void FunctionalArrayList(){}
     
     public ReturnObject head(){
-        ReturnObjectImpl returnItem = new ReturnObjectImpl(newList[0]);
-        return returnItem;
+        if(isEmpty()){
+            ReturnObjectImpl error = new ReturnObjectImpl(ErrorMessage.EMPTY_STRUCTURE);
+            return error;
+        } else {
+            ReturnObjectImpl returnItem = new ReturnObjectImpl(newList[0]);
+            return returnItem;
+        }
     }
     
-    public FunctionalList rest(){
-        
+    public FunctionalArrayList rest(){
+        if(isEmpty()){
+            FunctionalArrayList empty = new FunctionalArrayList();
+            return empty;
+        } else {
+            FunctionalArrayList returnList = new FunctionalArrayList();
+            for(int i = 1; i < arraySize; i++){
+                Object addItem = get(i).getReturnValue();
+                returnList.add(addItem);
+            }
+            return returnList;
+        }
     }
     
 }

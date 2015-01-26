4e456751a65e516fec6a88f91fb360f4559b5b5a
James <jamesghill@yahoo.co.uk>
2015-01-26 16:57:55 +0000

    Added JavaDoc

--- src/SampleableListImpl.java
+++ src/SampleableListImpl.java
@@ -1,25 +1,52 @@
+/**
+ * This is an implementation of SampleableList that extends ArrayList.
+ * 
+ * @author James
+ */
 public class SampleableListImpl extends ArrayList implements SampleableList  {
     
+    /**
+     * Class Constructor.
+     */
     public SampleableListImpl(){}
     
+    /**
+     * Class Constructor for ArrayList.
+     * 
+     * @param list 
+     */
     public SampleableListImpl(ArrayList list){
         for(int i = 0; i < list.size(); i++){
             add(list.get(i).getReturnValue());
         }
     }
     
+    /**
+     * Class Constructor for LinkedList.
+     * 
+     * @param list 
+     */
     public SampleableListImpl(LinkedList list){
         for(int i = 0; i < list.size(); i++){
             add(list.get(i).getReturnValue());
         }
     }
     
+    /**
+     * Class Constructor for FunctionalArrayList.
+     * 
+     * @param list 
+     */
     public SampleableListImpl(FunctionalArrayList list){
         for(int i = 0; i < list.size(); i++){
             add(list.get(i).getReturnValue());
         }
     }
     
+    /** Class Constructor for FunctionalLinkedList.
+     * 
+     * @param list 
+     */
     public SampleableListImpl(FunctionalLinkedList list){
         for(int i = 0; i < list.size(); i++){
             add(list.get(i).getReturnValue());

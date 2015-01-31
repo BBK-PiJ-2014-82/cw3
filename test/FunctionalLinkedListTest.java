a1c31e414b678be067a58c5dfcf155d2790e6e09
James <jamesghill@yahoo.co.uk>
2015-01-31 21:29:13 +0000

    Refactored into package 'cw3' from 'default package'.

--- test/FunctionalLinkedListTest.java
+++ test/FunctionalLinkedListTest.java
@@ -1,3 +1,7 @@
+import cw3.ReturnObject;
+import cw3.ErrorMessage;
+import cw3.FunctionalLinkedList;
+import cw3.FunctionalArrayList;
 import org.junit.*;
 import org.junit.Test;
 import static org.junit.Assert.*;

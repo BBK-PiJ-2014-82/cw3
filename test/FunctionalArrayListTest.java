8636715c88960aa8a7cea550726f268fca048dd4
James <jamesghill@yahoo.co.uk>
2015-01-31 23:03:36 +0000

    Made changes to the 'testHead' method.
    Deleted the test for getting head from empty array & wrote improved notation.

--- test/FunctionalArrayListTest.java
+++ test/FunctionalArrayListTest.java
@@ -4,6 +4,7 @@ import cw3.FunctionalArrayList;
 import org.junit.*;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import static cw3.ErrorMessage.*;
 
 public class FunctionalArrayListTest {
     
@@ -16,21 +17,24 @@ public class FunctionalArrayListTest {
     
     @Test
     public void testHead(){
+        // Check attempt to return head from empty array.
+        ReturnObject returnMsg = myArray.head();
+        assertEquals("Head return value is not empty.", EMPTY_STRUCTURE, returnMsg.getError());
+        
+        // Add 100 objects.
         for(int i = 0; i < 100; i++){
             myArray.add((i + 1) * 10);
         }
+        
+        // Check that the head is returned correctly.
         int headInt = (Integer) myArray.head().getReturnValue();
         assertEquals("Head return value is incorrect.", 10, headInt);
+        
+        // Check that the original array is unmodified.
         for(int i = 0; i < 100; i++){
             int j = (Integer) myArray.get(i).getReturnValue();
             assertEquals("Original array has been modified.", (i + 1) * 10, j);
         }
-    }
-    
-    @Test
-    public void testHeadIsEmpty(){
-        ReturnObject returnMsg = myArray.head();
-        assertEquals("Head return value is not empty.", ErrorMessage.EMPTY_STRUCTURE, returnMsg.getError());
     }
     
     @Test

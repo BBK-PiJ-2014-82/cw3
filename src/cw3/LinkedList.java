fab28ad05377dd0c7ebef3ff59b8917e7d266e50
James <jamesghill@yahoo.co.uk>
2015-01-31 22:05:43 +0000

    Modified the 'get' method.
    Added a new check for attempting to get results from empty lists.

--- src/cw3/LinkedList.java
+++ src/cw3/LinkedList.java
@@ -39,11 +39,15 @@ public class LinkedList implements List {
     
     @Override
     public ReturnObject get(int index){
+        ReturnObject returnItem;
         if(index < 0 || index > size){
-            ReturnObjectImpl error = new ReturnObjectImpl(INDEX_OUT_OF_BOUNDS);
-            return error;
+            if(size == 0){
+                returnItem = new ReturnObjectImpl(EMPTY_STRUCTURE);
+            } else {
+                returnItem = new ReturnObjectImpl(INDEX_OUT_OF_BOUNDS);
+            }
+            return returnItem;
         } else {
-            ReturnObjectImpl returnItem;
             Node runner = head;
             while(index != position){
                 runner = runner.next;

83ebe3cf5cd47188b90f96c6202be0d0d7bf630e
James <jamesghill@yahoo.co.uk>
2015-01-31 23:10:59 +0000

    Minor improvements & added notation to the 'testRest' method.

--- test/FunctionalArrayListTest.java
+++ test/FunctionalArrayListTest.java
@@ -1,10 +1,10 @@
 import cw3.ReturnObject;
-import cw3.ErrorMessage;
 import cw3.FunctionalArrayList;
 import org.junit.*;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import static cw3.ErrorMessage.*;
+import cw3.FunctionalList;
 
 public class FunctionalArrayListTest {
     
@@ -39,14 +39,21 @@ public class FunctionalArrayListTest {
     
     @Test
     public void testRest(){
+        FunctionalList restList;
+        
+        // Add 100 objects into the list.
         for(int i = 0; i < 100; i++){
             myArray.add((i + 1) * 10);
         }
-        FunctionalArrayList restList = myArray.rest();
+        
+        // Test rest function works correctly
+        restList = myArray.rest();
         for(int i = 0; i < 99; i++){
             int result = (Integer) restList.get(i).getReturnValue();
             assertEquals("Rest return value is incorrect.", (i + 2) * 10, result);
         }
+        
+        // Check that original array has not been modified.
         for(int i = 0; i < 100; i++){
             int j = (Integer) myArray.get(i).getReturnValue();
             assertEquals("Original array has been modified.", (i + 1) * 10, j);
@@ -54,7 +61,7 @@ public class FunctionalArrayListTest {
     }
     
     @Test
-    public void testRestReturnsEmpty(){
+    public void testReturnsEmpty(){
         boolean isEmpty = myArray.isEmpty();
         assertTrue("The newly created array is not empty.", isEmpty);
     }

be0a2c68337b5bb0c499c01c07ad10237473ed7e
James <jamesghill@yahoo.co.uk>
2015-01-31 21:34:05 +0000

    Refactored into package 'cw3' from 'default package'.

---
+++ src/cw3/StackImpl.java
@@ -0,0 +1,56 @@
+package cw3;
+
+/**
+ * This is an extension of AbstractStack.
+ * 
+ * @author James
+ */
+public class StackImpl extends AbstractStack {
+
+    /**
+     * Class Constructor.
+     * 
+     * @param list Any List that will be transformed into a stack.
+     */
+    public StackImpl(List list){
+        super(list);
+    }
+    
+    @Override
+    public boolean isEmpty() {
+        return internalList.isEmpty();
+    }
+
+    @Override
+    public int size() {
+        return internalList.size();
+    }
+
+    @Override
+    public void push(Object item) {
+        internalList.add(item);
+    }
+
+    @Override
+    public ReturnObject top() {
+        if(internalList.isEmpty()){
+            ReturnObjectImpl empty = new ReturnObjectImpl(ErrorMessage.EMPTY_STRUCTURE);
+            return empty;
+        } else {
+            ReturnObjectImpl item = new ReturnObjectImpl(internalList.get(0).getReturnValue());
+            return item;
+        }
+    }
+
+    @Override
+    public ReturnObject pop() {
+        if(internalList.isEmpty()){
+            ReturnObjectImpl empty = new ReturnObjectImpl(ErrorMessage.EMPTY_STRUCTURE);
+            return empty;
+        } else {
+            ReturnObjectImpl item = new ReturnObjectImpl(internalList.remove(0).getReturnValue());
+            return item;
+        }
+    }
+    
+}

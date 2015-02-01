7c44746fa49dde2dbefdd17cf0aa8b005c767add
James <jamesghill@yahoo.co.uk>
2015-02-01 01:59:32 +0000

    Added extra test and better notation.
    Added an extra test for the size of a stack after popping.

--- test/StackImplTest.java
+++ test/StackImplTest.java
@@ -9,24 +9,34 @@ public class StackImplTest {
     
     @Test
     public void testIsEmpty(){
+        // Create new stack.
         FunctionalLinkedList newList = new FunctionalLinkedList();
         stack = new StackImpl(newList);
+        
+        // Test new stack is empty.
         boolean empty = stack.isEmpty();
         assertTrue("New Stack is not empty.", empty);
+        
+        // Add element and check stack is not empty.
         stack.push(20);
         empty = stack.isEmpty();
         assertFalse("After push Stack is empty.", empty);
     }
     
     @Test
-    public void testSize(){
+    public void testSizeAndPop(){
+        // Create new list
         FunctionalLinkedList newList = new FunctionalLinkedList();
         for(int i = 0; i < 200; i++){
             newList.add(i * 10);
         }
         stack = new StackImpl(newList);
+        
+        // Check the size of the new stack.
         int size = stack.size();
         assertEquals("New Stack is wrong size.", 200, size);
+        
+        // Check the size after removing elements.
         for(int i = 100; i > 0; i--){
             stack.pop();
         }
@@ -36,11 +46,14 @@ public class StackImplTest {
     
     @Test
     public void testPush(){
+        // Create new stack.
         FunctionalLinkedList newList = new FunctionalLinkedList();
         for(int i = 0; i < 100; i++){
             newList.add(i * 10);
         }
         stack = new StackImpl(newList);
+        
+        // Push elements onto new stack and check size.
         for(int i = 100; i < 200; i++){
             stack.push(i * 10);
         }
@@ -50,25 +63,35 @@ public class StackImplTest {
     
     @Test
     public void testTop(){
+        // Create new stack.
         FunctionalLinkedList newList = new FunctionalLinkedList();
         for(int i = 1; i < 101; i++){
             newList.add(i * 15);
         }
         stack = new StackImpl(newList);
+        
+        // Check the top of the stack is correct value.
         int top = (Integer) stack.top().getReturnValue();
         assertEquals("Top result is incorrect.", 15, top);
     }
     
     @Test
     public void testPop(){
+        // Create a new stack.
         FunctionalLinkedList newList = new FunctionalLinkedList();
         for(int i = 1; i < 101; i++){
             newList.add(i * 15);
         }
         stack = new StackImpl(newList);
+        
+        // Check that popping an element leave the correct element on top.
         stack.pop();
         int pop = (Integer) stack.pop().getReturnValue();
         assertEquals("Initial pop did not work correctly.", 30, pop);
+        
+        // Check that the size after popping is correct.
+        int size = stack.size();
+        assertEquals("Stack is not the correct size.", 98, size);
     }
     
 }

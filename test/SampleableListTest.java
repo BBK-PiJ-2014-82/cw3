bdfeaf3dbc5aba7ae1f60611c229e26b1a023e0f
James <jamesghill@yahoo.co.uk>
2015-02-01 01:09:08 +0000

    Improved testing for FunctionalArrayList based sampleable lists.
    Adding notes and improved the testing.

--- test/SampleableListTest.java
+++ test/SampleableListTest.java
@@ -59,14 +59,26 @@ public class SampleableListTest {
     
     @Test
     public void SampleFunctionalArrayListTest(){
+        // Create initial list to be sampled.
         FunctionalArrayList myList = new FunctionalArrayList();
         for(int i = 1; i < 201; i++){
             myList.add(i * 10);
         }
-        SampleableListImpl newList = new SampleableListImpl(myList);
-        for(int i = 0; i < myList.size(); i++){
-            int j = (Integer) newList.get(i).getReturnValue();
-            assertEquals("Sampled numbers are incorrect.", (i+1) * 10, j);
+        
+        // Create sample of the initial list.
+        SampleableList newList;
+        newList = new SampleableListImpl(myList);
+        newList = newList.sample();
+        
+        // Test the size of the sampled list.
+        boolean sizeCheck = newList.size() == myList.size() / 2;
+        assertTrue("The new list is not a true sample.", sizeCheck);
+        
+        // Check that the correct numbers are returned.
+        int j = 0;
+        for(int i = 0; i < newList.size(); i++){
+            j = (Integer) newList.get(i).getReturnValue();
+            assertEquals("Sampled numbers are incorrect.", ((i*2)+1) * 10, j);
         }
     }
     
